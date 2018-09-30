985d9cd15e9c4bd16014be30a539efe168cb1309
sumitaka.sakuma <tksn.playthe69gt@docomo.ne.jp>
2018-09-30 16:52:07 +0000

    kadai-bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
-    before_action :set_task,only: [:show, :edit, :update, :destroy]
-     def index
-         @tasks= Task.all
+  before_action :set_task,only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks= Task.all.page(params[:page]).per(5)
   end
 
   def show
