577925862121590de1ff25ad93b1d9448af2149a
sumitaka.sakuma <tksn.playthe69gt@docomo.ne.jp>
2018-10-20 03:28:50 +0000

    kadai-twitter clone

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
 class TasksController < ApplicationController
-  before_action :set_task,only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks= Task.all.page(params[:page]).per(5)
+    @tasks= current_user.tasks.page(params[:page])
   end
 
   def show
-      set_task
   end
 
   def new
@@ -14,7 +14,7 @@ class TasksController < ApplicationController
   end
 
   def create
-     @task = Task.new(task_params)
+     @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
@@ -26,23 +26,21 @@ class TasksController < ApplicationController
 end
 
   def edit
-      set_task
   end
 
   def update
-      set_task
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
     end
   end
 
   def destroy
-      set_task
         @task.destroy
 
     flash[:success] = 'Taskは正常に削除されました'
@@ -54,6 +52,7 @@ end
   def set_task
      @task = Task.find(params[:id])
   end
+
    # Strong Parameter
   def task_params
      params.require(:task).permit(:content, :status)
